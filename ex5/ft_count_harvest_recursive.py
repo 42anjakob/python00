26fb53ce4794006f77780e57598df870bdfeea89
anjakob <anjakob@student.42heilbronn.de>
2026-02-27 17:09:46 +0100

    flake8 ready

--- ex5/ft_count_harvest_recursive.py
+++ ex5/ft_count_harvest_recursive.py
@@ -1,9 +1,10 @@
-def ft_recursive_function(days):
+def ft_recursive(days):
     if (days != 0):
-        ft_recursive_function(days - 1)
+        ft_recursive(days - 1)
         print("Day", days)
+
 
 def ft_count_harvest_recursive():
     days = int(input("Days until harvest: "))
-    ft_recursive_function(days)
+    ft_recursive(days)
     print("Harvest time!")
