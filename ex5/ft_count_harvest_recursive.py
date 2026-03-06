c7dc72cdfa94fcf7d2bbcc8ec0654fb8f131678a
anjakob <anjakob@student.42heilbronn.de>
2026-03-06 22:07:03 +0100

    Ouh the misery of to many unnecessary commits. They want to be... MY ENEMIES!

--- ex5/ft_count_harvest_recursive.py
+++ ex5/ft_count_harvest_recursive.py
@@ -1,13 +1,12 @@
 def ft_recursive(days: int = None) -> None:
-    if (days == None):
+    if days is None:
         return
-    elif (days != 0):
+    elif days != 0:
         ft_recursive(days - 1)
         print("Day", days)
 
 
 def ft_count_harvest_recursive() -> None:
     days = int(input("Days until harvest: "))
-    
     ft_recursive(days)
     print("Harvest time!")
